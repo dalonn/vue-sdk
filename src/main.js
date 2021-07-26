8cdaaae3e5100dc82853af90c7b8f193bf806499
dalonn <jinjiheyo@gmail.com>
2021-07-26 16:46:44 +0900

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// https//element--plus.org/ 화면으로 디자인 가능하게 설정
+import ElementPlus from 'element-plus'
+import 'element-;lus/lib/theme-chalk/index.css';
+
+createApp(App).use(ElementPlus).mount('#app')
